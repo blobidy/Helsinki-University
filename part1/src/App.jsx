2ce82f6666910a43589a9ef0b2ac96a7d645e61a
blobidy <bobucdeez@gmail.com>
2024-05-05 09:25:03 +0200

    Chenges

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,61 +1,46 @@
-const Header = ({course}) => {
+import React from 'react';
+
+const Header = ({ course }) => {
   return (
-    <div>
-      <h1>{course}</h1>
-    </div>
-  )
+    <h1>{course}</h1>
+  );
 }
 
-const Part = ({ part, exercises }) => {
-  return (
-    <p>{part} {exercises}</p>
-  )
-}
-
-const Content = ({ part1, exercises1, part2, exercises2, part3, exercises3 }) => {
+const Parts = ({ part1, part2, part3 }) => {
   return (
     <div>
-      <Part part={part1} exercises={exercises1} />
-      <Part part={part2} exercises={exercises2} />
-      <Part part={part3} exercises={exercises3} />
+      <p>{part1.name} {part1.exercises}</p>
+      <p>{part2.name} {part2.exercises}</p>
+      <p>{part3.name} {part3.exercises}</p>
     </div>
   );
-};
-
-
-
-const Total = ({ total }) => {
-  return (
-    <p>Number of exercises {total}</p>
-
-  )
 }
-  
-
-
-
-
 
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
-  const total = exercises1 + exercises2 + exercises3;
+  const course = 'Half Stack application development';
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
+
+  console.log(part1.name, part1.exercises, part2.name, part2.exercises, part3.name, part3.exercises)
 
   return (
     <div>
       <Header course={course} />
-      <Content part1={part1} exercises1={exercises1} />
-      <Content part2={part2} exercises2={exercises2} />
-      <Content part3={part3} exercises3={exercises3} />
-      <Total total={total} />
+      <Parts part1={part1} part2={part2} part3={part3} />
+      
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
