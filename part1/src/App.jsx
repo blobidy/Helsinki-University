1a279753ebcf2141d5e032a3eb970e326ca63ad0
blobidy <bobucdeez@gmail.com>
2024-05-05 09:53:55 +0200

    Done

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -17,14 +17,33 @@ const App = () => {
     ]
   }
 
+  const Header = ({ course }) => {
+    return (
+      <div>
+        <h1>{course.name}</h1>
+      </div>
+    )
+  }
+
+  //const Header = ({ course }) => {
+    //return (
+      //<div>
+       // {course.map(course => (
+        //  <h1>{course.name}</h1>
+       // ))}
+    //  </div>
+   // )
+ // }
   
+
+  console.log(course.name)
   console.log(course.parts[0].name, course.parts[0].exercises)
   console.log(course.parts[1].name, course.parts[1].exercises)
   console.log(course.parts[2].name, course.parts[2].exercises)
 
   return (
     <div>
-      ...
+      <Header course={course.name} />
     </div>
   )
 }
