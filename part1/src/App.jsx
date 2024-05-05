f9a9156a8baf172c224ac089e840d35c8d0877f5
blobidy <bobucdeez@gmail.com>
2024-05-05 09:41:52 +0200

    Making stuff

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,47 +1,32 @@
-import React from 'react';
-
-// Define Header component
-const Header = ({ course }) => {
-  return <h1>{course}</h1>;
-};
-
-// Define Parts component
-const Parts = ({ parts }) => {
-  return (
-    <div>
-      {parts.map((part, index) => (
-        <p key={index}>{part.name} {part.exercises}</p>
-      ))}
-    </div>
-  );
-};
-
 const App = () => {
-  const course = 'Half Stack application development';
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  };
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  };
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  };
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
-  // Array containing all parts
-  const allParts = [part1, part2, part3];
+  
+  console.log(course.parts[0].name, course.parts[0].exercises)
+  console.log(course.parts[1].name, course.parts[1].exercises)
+  console.log(course.parts[2].name, course.parts[2].exercises)
 
   return (
     <div>
-      {/* Render Header component */}
-      <Header course={course} />
-      {/* Render Parts component */}
-      <Parts parts={allParts} />
+      ...
     </div>
-  );
-};
+  )
+}
 
 export default App;
